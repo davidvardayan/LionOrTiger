5fe6342d02963b6952b7a5f9880b25fd646f18cf
David <dav-ss@mail.ru>
2019-06-22 11:04:42 -0700

    Adding layout and animations

--- app/src/main/java/com/davidvardanyan/lionortiger/MainActivity.java
+++ app/src/main/java/com/davidvardanyan/lionortiger/MainActivity.java
@@ -2,6 +2,8 @@ package com.davidvardanyan.lionortiger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void imageViewIsTapped(View imageView){
+
+        ImageView tappedImageView = (ImageView) imageView;
+        tappedImageView.setTranslationX(-2000);
+        tappedImageView.setImageResource(R.drawable.tiger);
+        tappedImageView.animate().translationXBy(2000).alpha(1).rotation(3600).setDuration(1000);
+
+    }
+
 }
